1d20584514db9c5da57f8f85458ab05b836c01be
sangamxp <160036687+sangamxp@users.noreply.github.com>
2025-03-30 23:07:11 +0545

    Graph insert and delete

---
+++ dsa/BinaryTreeCreation.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+struct node{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+struct node* root = NULL;
+struct node* InsertNode(struct node *node,int num){
+    if(node == NULL){
+        struct node *NewNode = (struct node*)malloc(sizeof(struct node));
+        NewNode -> data = num;
+        NewNode -> left = NULL;
+        NewNode -> right = NULL;
+        return NewNode;
+    }
+    if(num < node -> data){
+        node -> left = InsertNode(node->left, num);
+    }
+    else if(num > node -> data){
+        node -> right = InsertNode(node -> right, num);
+    }
+}
+void Display(struct node *node){
+    if(node == NULL){
+        return;
+    }
+    Display(node->left);
+    cout<<node->data<<" ";
+    Display(node->right);
+}
+struct node* deletenode(struct node* root, int key) {
+    if (root == NULL) return root;
+    if (key < root->data) {
+        root->left = deletenode(root->left, key);
+    } else if (key > root->data) {
+        root->right = deletenode(root->right, key);
+    } else {
+        if (root->left == NULL) {
+            struct node* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct node* temp = root->left;
+            free(root);
+            return temp;
+        }
+    }
+    return root;
+}
+int main() {
+    root = InsertNode(root, 50);
+    root = InsertNode(root, 30);
+    root = InsertNode(root, 70);
+    root = InsertNode(root, 20);
+    root = InsertNode(root, 40);
+    root = InsertNode(root, 35);
+    root = InsertNode(root, 45);
+    root = InsertNode(root, 60);
+    root = InsertNode(root, 80);
+    root = deletenode(root, 20);
+
+    cout << "Inorder Traversal of BST: ";
+    Display(root);
+    cout << endl;
+
+    return 0;
+}
